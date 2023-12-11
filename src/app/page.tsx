445aa0f506be74f5fade69e89b63723c3e7a667b
Matilda007 <matilda230730@gmail.com>
2023-12-11 12:14:19 +0900

    [EDIT] optioning SCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import "./globals.scss";
 
 export default function Home() {
-	return <></>;
+	return (
+		<>
+			<div></div>
+		</>
+	);
 }
