c24da16430bfa8fa7b6cb2d5e128dd3a0bf4b98c
HermannChoi <hiyunseok347@gmail.com>
2023-12-12 18:52:12 +0900

    [EDIT]sidebar editted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,25 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function Home() {
   const [isNone, setIsNone] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <div className={styles.pageBody}>
-      <Sidebar />
-      <div className={styles.main_body}>
-        <CreateMemo />
-        {isNone ? (
-          <div className={styles.explanation}>
-            <div className={`${styles.first_icon} material-symbols-outlined`}>
-              lightbulb
-            </div>
-            <span className={styles.first_text}>
-              추가한 메모가 여기에 표시됩니다.
-            </span>
+    <div className={styles.main_body}>
+      <CreateMemo />
+      {isNone ? (
+        <div className={styles.explanation}>
+          <div className={`${styles.first_icon} material-symbols-outlined`}>
+            lightbulb
           </div>
-        ) : null}
-      </div>
+          <span className={styles.first_text}>
+            추가한 메모가 여기에 표시됩니다.
+          </span>
+        </div>
+      ) : null}
     </div>
   );
 }
