bd295e466372fdf876db2ccc735d1681111008e8
HermannChoi <hiyunseok347@gmail.com>
2023-12-14 19:15:06 +0900

    [ADD]modalAdded

---
+++ src/app/components/Sidebar/ModalState/ModalState.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import styles from "./ModalState.module.scss";
+
+const ModalState = () => {
+  return <></>;
+};
+
+export default ModalState;
--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -7,6 +7,10 @@ import Navbar from "../../Navbar/Navbar";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
 import { toggleSidebar } from "../../../redux/features/sidebar/sidebarSlice";
+import ModalState from "./ModalState/ModalState";
+import Modal from "react-modal";
+import { IoMdAdd } from "react-icons/io";
+import { IoMdCheckmark } from "react-icons/io";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -16,6 +20,22 @@ const Sidebar = () => {
 
   const sidebarItems = useSelector((state: RootState) => state.menu.items);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const customStyles = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+  };
+
   return (
     <div
       className={`${styles.sidebar_space} ${
@@ -24,21 +44,64 @@ const Sidebar = () => {
     >
       {sidebarItems.map((page, idx) => {
         return (
-          <Link
-            key={idx}
-            id={styles.focus}
-            href={page.link}
-            className={styles.sidebar_menu}
-          >
-            <div
-              className={`${styles.sidebar_icons} material-symbols-outlined`}
-            >
-              {page.iconName}
-            </div>
-            <span className={page.textClass}> {page.name}</span>
-          </Link>
+          <>
+            {idx === 2 ? (
+              <div
+                key={idx}
+                className={styles.sidebar_menu}
+                onClick={openModal}
+              >
+                <div
+                  className={`${styles.sidebar_icons} material-symbols-outlined`}
+                >
+                  {page.iconName}
+                </div>
+                <span className={page.textClass}>{page.name}</span>
+              </div>
+            ) : (
+              <Link href={page.link} key={idx} className={styles.sidebar_menu}>
+                <div
+                  className={`${styles.sidebar_icons} material-symbols-outlined`}
+                >
+                  {page.iconName}
+                </div>
+                <span className={page.textClass}>{page.name}</span>
+              </Link>
+            )}
+          </>
         );
       })}
+
+      <div className={styles.ReactModal__Overlay}>
+        <div className={styles.ReactModal__Content}>
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            className={styles.modal_container}
+            style={customStyles}
+          >
+            <div className={styles.modal_space}>
+              <div className={styles.modal_upperSpace}>
+                <span className={styles.modal_title}>라벨 수정</span>
+                <div className={styles.modal_inputspace}>
+                  <IoMdAdd className={styles.add} />
+                  <input
+                    type="text"
+                    className={styles.input}
+                    placeholder="새 라벨 만들기"
+                  />
+                  <IoMdCheckmark className={styles.make} />
+                </div>
+              </div>
+              <div className={styles.modal_downSpace}>
+                <button onClick={closeModal} className={styles.closingBtn}>
+                  완료
+                </button>
+              </div>
+            </div>
+          </Modal>
+        </div>
+      </div>
     </div>
   );
 };
