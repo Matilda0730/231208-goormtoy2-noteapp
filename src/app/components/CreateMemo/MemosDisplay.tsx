7c7b53daa18bb5badafe79da03b5408847f19fe3
YunseokChoi <hiyunseok347@gmail.com>
2024-01-03 18:14:27 +0900

    [EDIT]pinFunction(notFinishedYet)

--- src/app/components/CreateMemo/MemosDisplay.tsx
+++ src/app/components/CreateMemo/MemosDisplay.tsx
@@ -1,86 +1,245 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Note } from "app/models/note";
 import styles from "./MemosDisplay.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "reduxprops/store/store";
 import { togglePaletteModal } from "@slice/modal/modalSlice";
 import ColorModal from "./ColorModal";
-import { moveToArchive, moveToTrashCan } from "@slice/memo/notesSlice";
+import {
+  actualPinToggle,
+  moveToArchive,
+  moveToTrashCan,
+} from "@slice/memo/notesSlice";
 
 const MemosDisplay = () => {
-  const memos = useSelector((state: RootState) => state.notes.notes);
+  const [pinClicked, setPinClicked] = useState<boolean>(false);
 
   const dispatch = useDispatch();
+  const memos = useSelector((state: RootState) => state.notes.notes);
+  const pinnedNotes = memos.filter((notes) => notes.isPinned);
+  const normalNotes = memos.filter((notes) => !notes.isPinned);
 
-  const handleToggleModal = () => {
-    dispatch(togglePaletteModal());
+  const pinToggle = () => {
+    setPinClicked(!pinClicked);
   };
 
   return (
     <>
-      <div className={styles.pin_space}></div>
       <div className={styles.memo_space}>
         {memos.length > 0 ? (
-          memos.map((memo) => (
-            <div
-              key={memo.id}
-              className={styles.memoContainer}
-              style={{ backgroundColor: memo.backgroundColor }}
-            >
-              <div key={memo.id} className={styles.memo}>
-                <h3 style={{ marginBottom: "10px" }}>{memo.title}</h3>
-                <p>{memo.text}</p>
-                <div
-                  id={styles.push_pin}
-                  className={`material-symbols-outlined`}
-                >
-                  push_pin
-                </div>
+          pinnedNotes.length > 0 ? (
+            <div className={styles.pinSituation}>
+              <div className={styles.standsText}>고정됨</div>
+              <div className={styles.Notes_space}>
+                {pinnedNotes.map((memo) => (
+                  <div
+                    key={memo.id}
+                    className={styles.memoContainer}
+                    style={{ backgroundColor: memo.backgroundColor }}
+                  >
+                    <div key={memo.id} className={styles.memo}>
+                      <h3 style={{ marginBottom: "10px" }}>{memo.title}</h3>
+                      <p>{memo.text}</p>
+                      <div
+                        id={styles.push_pin}
+                        className={`${
+                          pinClicked
+                            ? `material-icons`
+                            : `material-symbols-outlined`
+                        }`}
+                        onClick={() => {
+                          pinToggle();
+                          dispatch(actualPinToggle(memo.id));
+                        }}
+                      >
+                        push_pin
+                      </div>
+                    </div>
+                    <div className={styles.memoButton_space}>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                      >
+                        add_alert
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        // onClick={handleToggleModal}
+                      >
+                        palette
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        onClick={() => {
+                          dispatch(moveToArchive(memo));
+                        }}
+                      >
+                        archive
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        // onClick={handleToggleLabelModal}
+                      >
+                        label
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        onClick={() => {
+                          dispatch(moveToTrashCan(memo));
+                        }}
+                      >
+                        delete
+                      </div>
+                    </div>
+                    {/* <ColorModal /> */}
+                  </div>
+                ))}
               </div>
-              <div className={styles.memoButton_space}>
-                <div
-                  id={styles.bottom_icons}
-                  className={`material-symbols-outlined`}
-                >
-                  add_alert
-                </div>
-                <div
-                  id={styles.bottom_icons}
-                  className={`material-symbols-outlined`}
-                  // onClick={handleToggleModal}
-                >
-                  palette
-                </div>
-                <div
-                  id={styles.bottom_icons}
-                  className={`material-symbols-outlined`}
-                  onClick={() => {
-                    dispatch(moveToArchive(memo));
-                  }}
-                >
-                  archive
-                </div>
-                <div
-                  id={styles.bottom_icons}
-                  className={`material-symbols-outlined`}
-                  // onClick={handleToggleLabelModal}
-                >
-                  label
-                </div>
-                <div
-                  id={styles.bottom_icons}
-                  className={`material-symbols-outlined`}
-                  onClick={() => {
-                    dispatch(moveToTrashCan(memo));
-                  }}
-                >
-                  delete
-                </div>
+              <div className={styles.standsText}>기타</div>
+              <div className={styles.Notes_space}>
+                {normalNotes.map((memo) => (
+                  <div
+                    key={memo.id}
+                    className={styles.memoContainer}
+                    style={{ backgroundColor: memo.backgroundColor }}
+                  >
+                    <div key={memo.id} className={styles.memo}>
+                      <h3 style={{ marginBottom: "10px" }}>{memo.title}</h3>
+                      <p>{memo.text}</p>
+                      <div
+                        id={styles.push_pin}
+                        className={`${
+                          pinClicked
+                            ? `material-icons`
+                            : `material-symbols-outlined`
+                        }`}
+                        onClick={() => {
+                          pinToggle();
+                          dispatch(actualPinToggle(memo.id));
+                        }}
+                      >
+                        push_pin
+                      </div>
+                    </div>
+                    <div className={styles.memoButton_space}>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                      >
+                        add_alert
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        // onClick={handleToggleModal}
+                      >
+                        palette
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        onClick={() => {
+                          dispatch(moveToArchive(memo));
+                        }}
+                      >
+                        archive
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        // onClick={handleToggleLabelModal}
+                      >
+                        label
+                      </div>
+                      <div
+                        id={styles.bottom_icons}
+                        className={`material-symbols-outlined`}
+                        onClick={() => {
+                          dispatch(moveToTrashCan(memo));
+                        }}
+                      >
+                        delete
+                      </div>
+                    </div>
+                    {/* <ColorModal /> */}
+                  </div>
+                ))}
               </div>
-              {/* <ColorModal /> */}
             </div>
-          ))
+          ) : (
+            memos.map((memo) => (
+              <div
+                key={memo.id}
+                className={styles.memoContainer}
+                style={{ backgroundColor: memo.backgroundColor }}
+              >
+                <div key={memo.id} className={styles.memo}>
+                  <h3 style={{ marginBottom: "10px" }}>{memo.title}</h3>
+                  <p>{memo.text}</p>
+                  <div
+                    id={styles.push_pin}
+                    className={`${
+                      pinClicked
+                        ? `material-icons`
+                        : `material-symbols-outlined`
+                    }`}
+                    onClick={() => {
+                      pinToggle();
+                      dispatch(actualPinToggle(memo.id));
+                    }}
+                  >
+                    push_pin
+                  </div>
+                </div>
+                <div className={styles.memoButton_space}>
+                  <div
+                    id={styles.bottom_icons}
+                    className={`material-symbols-outlined`}
+                  >
+                    add_alert
+                  </div>
+                  <div
+                    id={styles.bottom_icons}
+                    className={`material-symbols-outlined`}
+                    // onClick={handleToggleModal}
+                  >
+                    palette
+                  </div>
+                  <div
+                    id={styles.bottom_icons}
+                    className={`material-symbols-outlined`}
+                    onClick={() => {
+                      dispatch(moveToArchive(memo));
+                    }}
+                  >
+                    archive
+                  </div>
+                  <div
+                    id={styles.bottom_icons}
+                    className={`material-symbols-outlined`}
+                    // onClick={handleToggleLabelModal}
+                  >
+                    label
+                  </div>
+                  <div
+                    id={styles.bottom_icons}
+                    className={`material-symbols-outlined`}
+                    onClick={() => {
+                      dispatch(moveToTrashCan(memo));
+                    }}
+                  >
+                    delete
+                  </div>
+                </div>
+                {/* <ColorModal /> */}
+              </div>
+            ))
+          )
         ) : (
           <div className={styles.explanation}>
             <div className={`${styles.first_icon} material-symbols-outlined`}>
